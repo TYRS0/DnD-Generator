a79dc884ed7b5f869c5e3caaf42ab66ae59e2ad4
Thomas Yoder <66986942+TYRS0@users.noreply.github.com>
2023-12-20 21:02:34 -0800

    v 0.0.2
    Got Race and SubRace generation working

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 from random import randint, choice
 from json import load
 
+
+
 def Generate_Class():
     Class_File = open("Classes.json", "r")
     Class_Json = load(Class_File)
@@ -8,8 +10,25 @@ def Generate_Class():
     Class = (Class_Json[randint(0,Class_Length)])
     return Class
 
+def Generate_Race():
+    Race_File = open("Races.json", "r")
+    Race_Json = load(Race_File)
+    Race_Length = (len(Race_Json["Races"])-1)
+    Race = (Race_Json["Races"][randint(0,Race_Length)])
+    SubRace_Length = (len(Race_Json[Race])-1)
+    SubRace = (Race_Json[Race][randint(0,SubRace_Length)])
+    return Race, SubRace
+
+
 
 Class = Generate_Class()
+Race, SubRace = Generate_Race()
 
+
+
+if SubRace == "N/A":
+    print (Race)
+else:
+    print (SubRace + " " + Race)
 
 print (Class)
