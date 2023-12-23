300237be1d0ab6f673e70d084b04cc149a640c37
Thomas Yoder <66986942+TYRS0@users.noreply.github.com>
2023-12-23 00:40:51 -0800

    v 0.0.3
    Name Generation works and is almost finished, Names for Dragonborn, Half Elf, Half Orc, Humans, and Tiefling need to be finished

--- Main.py
+++ Main.py
@@ -19,16 +19,31 @@ def Generate_Race():
     SubRace = (Race_Json[Race][randint(0,SubRace_Length)])
     return Race, SubRace
 
+def Generate_Name():
+    Name_File = open("Names.json", "r")
+    Name_Json = load(Name_File)
+    Gender_Length = (len(Name_Json["Gender"])-1)
+    Gender = (Name_Json["Gender"][randint(0,Gender_Length)])
+    First_Name_Length = (len(Name_Json[Race + " " + Gender])-1)
+    Last_Name_Length = (len(Name_Json[Race + " Last"])-1)
+    First_Name = (Name_Json[Race + " " + Gender][randint(0,First_Name_Length)])
+    Last_Name = (Name_Json[Race + " Last"][randint(0,Last_Name_Length)])
+    return First_Name, Last_Name, Gender
+
 
 
 Class = Generate_Class()
 Race, SubRace = Generate_Race()
+First_Name, Last_Name, Gender = Generate_Name()
 
 
+
+print ("Name: " + First_Name +" "+ Last_Name)
+print ("Gender: " + Gender)
 
 if SubRace == "N/A":
-    print (Race)
+    print ("Race: "+ Race)
 else:
-    print (SubRace + " " + Race)
+    print ("Race: " + SubRace + " " + Race)
 
-print (Class)
+print ("Class: "+ Class)
