0936a95ba18df83169a047059e668eb1ca07f7b0
Thomas Yoder <66986942+TYRS0@users.noreply.github.com>
2024-01-01 10:25:03 -0800

    v 0.0.4
    Finished off all Names, Modified Name Generation for Half Races using multiple Pools of names, and Added the forgotten Gnomes

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from random import randint, choice
 from json import load
 
 
-
+#Generators
 def Generate_Class():
     Class_File = open("Classes.json", "r")
     Class_Json = load(Class_File)
@@ -23,22 +23,35 @@ def Generate_Name():
     Name_File = open("Names.json", "r")
     Name_Json = load(Name_File)
     Gender_Length = (len(Name_Json["Gender"])-1)
-    Gender = (Name_Json["Gender"][randint(0,Gender_Length)])
-    First_Name_Length = (len(Name_Json[Race + " " + Gender])-1)
-    Last_Name_Length = (len(Name_Json[Race + " Last"])-1)
-    First_Name = (Name_Json[Race + " " + Gender][randint(0,First_Name_Length)])
-    Last_Name = (Name_Json[Race + " Last"][randint(0,Last_Name_Length)])
+    Gender = (Name_Json["Gender"][randint(0,Gender_Length)]) 
+
+    if Race in Name_Json["Pools"]:
+        Race_Name_Pool = (len(Name_Json[Race + " Race Pool"])-1)
+        Race_Selction = (Name_Json[Race + " Race Pool"][randint(0,Race_Name_Pool)])
+        First_Name_Length = (len(Name_Json[Race_Selction + " " + Gender])-1)
+        Last_Name_Length = (len(Name_Json[Race_Selction + " Last"])-1)
+        First_Name = (Name_Json[Race_Selction + " " + Gender][randint(0,First_Name_Length)])
+        Last_Name = (Name_Json[Race_Selction + " Last"][randint(0,Last_Name_Length)])
+    else:   
+        First_Name_Length = (len(Name_Json[Race + " " + Gender])-1)
+        Last_Name_Length = (len(Name_Json[Race + " Last"])-1)
+        First_Name = (Name_Json[Race + " " + Gender][randint(0,First_Name_Length)])
+        Last_Name = (Name_Json[Race + " Last"][randint(0,Last_Name_Length)])
     return First_Name, Last_Name, Gender
 
 
-
+#Generation
 Class = Generate_Class()
 Race, SubRace = Generate_Race()
 First_Name, Last_Name, Gender = Generate_Name()
 
 
-
-print ("Name: " + First_Name +" "+ Last_Name)
+#Output
+if Last_Name == "N/A":
+    print ("Name: " + First_Name)
+else:
+    print ("Name: " + First_Name +" "+ Last_Name)
+    
 print ("Gender: " + Gender)
 
 if SubRace == "N/A":
